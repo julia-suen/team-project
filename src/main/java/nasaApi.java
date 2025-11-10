3efe9fdb4e5067eb655c3fe367bf0b8455adefab
Angeline <angelinehowcl@gmail.com>
2025-11-10 16:26:23 -0500

    trying to read csv and load api call

--- src/main/java/nasaApi.java
+++ src/main/java/nasaApi.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
 import java.io.IOException;
 import java.util.*;
 
 public class nasaApi {
-
 
     /**
      * BreedFetcher implementation that relies on the dog.ceo API.
@@ -20,43 +22,28 @@ public class nasaApi {
      * exceptions to align with the requirements of the BreedFetcher interface.
      */
 
-    private static final String MESSAGE = "message";
-    private static final String STATUS = "status";
-    private static final String SUCCESS = "success";
+    private static final String MAP_KEY = "2f1f3b83b749cc2829c806c0e8e8959c";
+    private static final String SOURCE = "VIIRS_SNPP_NRT";
+    private static final String REGION = "world";
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
      *
-     * @param breed the breed to fetch sub breeds for
-     * @return list of sub breeds for the given breed
-     * @throws lol if the breed does not exist (or if the API call fails for any reason)
+     * @param dateRange an integer 1-10 specifying the number of days to fetch data for
+     * @param date a start date from which to fetch data for
+     * @return dictionary with keys mapped to values needed (???)
+
      */
-    @Override
-    public List<String> getSubBreeds(String breed) throws DataNotFoundException {
+
+    public List<String> getFireData(int dateRange, String date) {
+        //consider returning a dictionary with keys diff attributes and associated values?
         final OkHttpClient client = new OkHttpClient();
-        final String request_url = "https://dog.ceo/api/breed/" + breed + "/list";
+        final String request_url = "https://firms.modaps.eosdis.nasa.gov/usfs/api/area/csv/" + MAP_KEY + "/" + SOURCE +
+                "/" + REGION + dateRange + "/" + date;
         final Request request = new Request.Builder().url(request_url).build();
 
-        try {
-            Response response = client.newCall(request).execute();
-            JSONObject responseBody = new JSONObject(response.body().string());
-
-            if (Objects.equals(responseBody.getString(STATUS), SUCCESS)) {
-                final JSONArray subBreeds = responseBody.getJSONArray(MESSAGE);
-                List<String> subBreedList = new ArrayList<>();
-
-                for (int i = 0; i < subBreeds.length(); i++) {
-                    subBreedList.add(subBreeds.getString(i));
-                }
-                return subBreedList;
-            } else {
-                throw new BreedNotFoundException(breed);
-            }
-
-        } catch (Exception e) {
-            throw new BreedNotFoundException(breed);
-
-        }
-
+        // extract data from csv:
+        // double latitude, double longitude, double brightTemp,
+        return [];
     }
 }
