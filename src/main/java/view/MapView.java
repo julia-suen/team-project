fb177be3cac63b97ce1c0d8a4e5b6b04cf4fd633
Joseph Cheung <65944201+JosephCheung0327@users.noreply.github.com>
2025-11-15 17:03:20 -0500

    update

--- src/main/java/view/MapView.java
+++ src/main/java/view/MapView.java
@@ -10,30 +10,40 @@ import java.awt.*;
 
 public class MapView extends JFrame {
 
-    public MapView() {
-        super("JXMapKit - Canada");
+  public MapView() {
+    super("JXMapKit - Canada");
 
-        // Create JXMapKit
-        JXMapKit mapKit = new JXMapKit();
+    // Create JXMapKit
+    JXMapKit mapKit = new JXMapKit();
 
-        // Use OpenStreetMap tiles
-        OSMTileFactoryInfo info = new OSMTileFactoryInfo();
-        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
-        mapKit.setTileFactory(tileFactory);
+    // Use OpenStreetMap tiles
+    OSMTileFactoryInfo info = new OSMTileFactoryInfo();
+    DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+    mapKit.setTileFactory(tileFactory);
 
-        // Center the map on Canada
-        GeoPosition canada = new GeoPosition(56.1304, -106.3468);
-        mapKit.setAddressLocation(canada);
+    // Adjust buttons size for zoom in/out
+    Dimension buttonSize = new Dimension(40, 40);
 
-        // Zoom level (0=world)
-        mapKit.setZoom(4);
+    JButton zoomIn = mapKit.getZoomInButton();
+    JButton zoomOut = mapKit.getZoomOutButton();
 
-        // Add to window
-        add(mapKit, BorderLayout.CENTER);
+    zoomIn.setPreferredSize(buttonSize);
+    zoomOut.setPreferredSize(buttonSize);
 
-        setSize(900, 700);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
+
+    // Center the map on Canada
+    GeoPosition canada = new GeoPosition(56.1304, -106.3468);
+    mapKit.setAddressLocation(canada);
+
+    // Zoom level (0=world)
+    mapKit.setZoom(4);
+
+    // Add to window
+    add(mapKit, BorderLayout.CENTER);
+
+    setSize(900, 700);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setVisible(true);
+  }
 }
