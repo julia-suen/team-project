c4960618dee0a2b1d509745301b86caaeb84634e
Perseus-D <shangyichendong@gmail.com>
2025-11-24 17:37:00 -0500

    fix the new bugs appearing after merging

--- src/test/java/BoundariesDataAccessTest.java
+++ src/test/java/BoundariesDataAccessTest.java
@@ -1,23 +1,23 @@
-// import data_access.BoundariesDataAccess;
-// import data_access.GetData;
-// import entities.Region;
-// import org.junit.jupiter.api.Test;
-//
-// import java.util.List;
-//
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-// public class BoundariesDataAccessTest {
-//
-//     @Test
-//     public void testLoadOneProvince() throws Exception {
-//         BoundariesDataAccess dao = new BoundariesDataAccess();
-//         List<List<org.jxmapviewer.viewer.GeoPosition>> boundaries = dao.getBoundariesData("Ontario");
-//         assertNotNull(boundaries);
-//
-//         Region region = new Region("Ontario", boundaries);
-//         assertEquals("Ontario", region.getProvinceName());
-//         assertEquals(boundaries, region.getBoundary());
-//     }
-// }
+import data_access.BoundariesDataAccess;
+import data_access.GetData;
+import entities.Region;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BoundariesDataAccessTest {
+
+    @Test
+    public void testLoadOneProvince() throws Exception {
+        BoundariesDataAccess dao = new BoundariesDataAccess();
+        List<List<org.jxmapviewer.viewer.GeoPosition>> boundaries = dao.getBoundariesData("Ontario");
+        assertNotNull(boundaries);
+
+        Region region = new Region("Ontario", boundaries);
+        assertEquals("Ontario", region.getProvinceName());
+        assertEquals(boundaries, region.getBoundary());
+    }
+}
